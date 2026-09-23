65ea46c7b3b6551e211cc79719435281c3613371
MEER <akhilmeer@hotmail.com>
2026-09-23 19:52:13 +0530

    added normalization to it

--- main.cpp
+++ main.cpp
@@ -42,34 +42,72 @@
 #include <stdexcept>
 #include <string>
 #include <vector>
+#include <cmath>
 
 #include "tokenizers_cpp.h"
 
 
 using namespace std;
 
-std::string load_file(const std::string& path)
-{
-    std::ifstream file(path, std::ios::binary);
+// std::string load_file(const std::string& path)
+// {
+//     std::ifstream file(path, std::ios::binary);
 
-    if (!file) {
-        throw std::runtime_error("Could not open: " + path);
-    }
+//     if (!file) {
+//         throw std::runtime_error("Could not open: " + path);
+//     }
 
-    return std::string(
-        std::istreambuf_iterator<char>(file),
-        std::istreambuf_iterator<char>()
-    );
-}
+//     return std::string(
+//         std::istreambuf_iterator<char>(file),
+//         std::istreambuf_iterator<char>()
+//     );
+// }
 
 // overall flow is like i have vector of ids, now i need to look up the ids to embeddings, the strore those in a tenor = vector<vector<float>>, then while porcessing 
 // x= wte + wpe  for the first transformer head, it is basically take embedding e then do x=wte+wpe  then x*h0= {q,k,v} , we need to do causality that means only 
 // look at previous tokens in attention, so we need to update v, q*k of everything  and then we have score of all, then do v=score*(other values)
 // do this for every iteration for one block , at the end we need to send to mlp, which is basically  
 //using tensor=vector<float>;
-vector<vector<float>> do_matadd(vector<vector<float>>& embedding,vector<vector<float>>& wte,vector<vector<float>>& wpe){
-    //
+
+
+void LayerNorm(vector<vector<float>>& embeddings){// need mean and variance and then load learned parameters
+    //we need to do row-wise
+    float mean=0;
+    for(int i=0;i<embeddings.size();i++){
+        for(int j=0;j<embeddings[0].size();j++){
+            mean+=embeddings[i][j];
+        }
+        mean=mean/768;
+        //now do variance
+        float variance=0;
+        for(int j=0;j<embeddings.size();j++){
+            variance+=(embeddings[i][j]-mean)*(embeddings[i][j]-mean);
+        }
+        variance=variance/768;
+        // then normalize x̂ᵢ = (xᵢ - mean) / sqrt(variance + ε)
+        float eps=1e-5;
+        for(int j=0;j<embeddings[0].size();j++){
+            embeddings[i][j]=  (embeddings[i][j] - mean)/sqrt(variance+eps);
+        }
+    }
+    // i think this does the normalisation, then we need to do the learned weights : n*ln1_w + ln_1b
 }
+
+
+void do_rowadd(vector<float> &a, vector<float>& b){ // add rows of matrix
+    for(int i=0;i<a.size();i++){
+        a[i]+=b[i];
+    }
+}
+
+void do_matadd(vector<vector<float>>& embedding,vector<vector<float>>& wpe){
+    for(int i=0;i<embedding.size();i++){
+        do_rowadd(embedding[i],wpe[i]);
+    }
+}
+
+
+
 
 vector<vector<float>> do_embed(vector<int32_t>& id){
     //logic to return embedding
@@ -77,10 +115,12 @@ vector<vector<float>> do_embed(vector<int32_t>& id){
     vector<vector<float>> wte(50257,vector<float>(768,0));
     vector<vector<float>> wpe(1024,vector<float>(768,0));
     
+    
+    //loading wte
     ifstream file("weights/transformer.wte.weight.txt");
     if(!file){
         cout<<"error\n";
-        return wte;   // debug this
+        return wte;  // debug this
     }
     float num;
     // for wte
@@ -90,22 +130,26 @@ vector<vector<float>> do_embed(vector<int32_t>& id){
             wte[i][j]=num;
         }
     }
+    file.close();
     ifstream file("weights/transformer.wpe.weight.txt");
+    
+    
     //for wpe
-    for(int i=0;i<50257;i++){ 
+    for(int i=0;i<1024;i++){ 
         for(int j=0;j<768;j++){
-            cin>>num;
+            file>>num;
             wpe[i][j]=num;
         }
     }
+    file.close();
     cout<<" wte and wpe loaded!"<<endl;
     // so basically we need to return wte + wpe, tokens less than 1024 for now
-    vector<float> embedding;
+    vector<vector<float>> embedding;
     for(int i=0;i<id.size();i++){
         embedding.push_back( wte[id[i]] );
     }
-
-    return wte;
+    do_matadd(embedding,wpe);
+    return embedding;
 }
 
 
@@ -130,12 +174,13 @@ vector<int32_t> init_tokenizer(string s){
 }
 
 int main(){   
-    string s="I like the cat is veryhappy"; //  for now keep it below 1024 tokens because of wpe restrictions
+    string s="I like the cat is very happy"; //  for now keep it below 1024 tokens because of wpe restrictions
 
 
     vector<int32_t> id=init_tokenizer(s);// i give string and get vector of ids
-    vector<vector<float>> embeddings=do_embed(id);
-
-
+    vector<vector<float>> embeddings=do_embed(id); // i give vector of ids and get return of embeddings(x) with positional addition also
+    LayerNorm(embeddings);
+    // i think this does the normalisation, then we need to do the learned weights : n*ln1_w + ln_1b
+    cout<<"done till lates"<<endl;
     return 0;
 }
